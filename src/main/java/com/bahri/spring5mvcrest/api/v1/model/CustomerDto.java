3a5086d8bd613faa77c12f741c7ffac04dbf93dd
Bahri <raed.bahri@esprit.tn>
2021-10-14 00:19:46 +0200

    Added Customer Post Api(Controller, Service, Repository,Unit tests)

--- src/main/java/com/bahri/spring5mvcrest/api/v1/model/CustomerDto.java
+++ src/main/java/com/bahri/spring5mvcrest/api/v1/model/CustomerDto.java
@@ -1,10 +1,13 @@
 package com.bahri.spring5mvcrest.api.v1.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
     private String firstName;
     private String lastName;
+
+    @JsonProperty("customer_url")
     private String customerUrl;
 }
