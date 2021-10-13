4551c66ac23d08ae6f39f2a5d57f13346b954cc4
Bahri <raed.bahri@esprit.tn>
2021-10-13 17:58:47 +0200

    Fix Context Loads

--- src/main/java/com/bahri/spring5mvcrest/domain/Category.java
+++ src/main/java/com/bahri/spring5mvcrest/domain/Category.java
@@ -5,17 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Category {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/bahri/spring5mvcrest/repositories/CategoryRepository.java
+++ src/main/java/com/bahri/spring5mvcrest/repositories/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.bahri.spring5mvcrest.repositories;
 
 import com.bahri.spring5mvcrest.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
