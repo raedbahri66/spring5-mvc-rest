661d307ce4f44a6045051768367ba66f899ec548
Bahri <raed.bahri@esprit.tn>
2021-10-13 17:47:44 +0200

    Initial commit

---
+++ src/main/java/com/bahri/spring5mvcrest/api/v1/model/CategoryDto.java
@@ -0,0 +1,6 @@
+package com.bahri.spring5mvcrest.api.v1.model;
+
+public class CategoryDto {
+    private  Long id;
+    private String name;
+}
---
+++ src/main/java/com/bahri/spring5mvcrest/domain/Category.java
@@ -0,0 +1,24 @@
+package com.bahri.spring5mvcrest.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
